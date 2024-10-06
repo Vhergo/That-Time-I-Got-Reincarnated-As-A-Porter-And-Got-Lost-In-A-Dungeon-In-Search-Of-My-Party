7c52fe0b2dfec64a78309a8920be42aacddea2cd
Bemister Tessema <BTessemaDev@gmail.com>
2024-10-06 16:54:27 -0700

    Most Spider Region Assets Added

--- Assets/_Scripts/Environment/Chain.cs
+++ Assets/_Scripts/Environment/Chain.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 public class Chain : MonoBehaviour
 {
     [SerializeField] private GameObject chainLinkPrefab;
+    [SerializeField] private GameObject chainEndingPrefab;
     [SerializeField] private int chainLength;
     [SerializeField] private float linkOffset = -0.35f;
+    [SerializeField] private bool doubleEnded;
+
+    [Space(10)]
     [SerializeField] private List<GameObject> chain;
     private GameObject anchor;
     private Transform previousLink;
@@ -25,7 +29,8 @@ public class Chain : MonoBehaviour
         for (int i = 0; i < chainLength; i++) {
             Vector3 newPos = previousLink.position;
             if (i != 0) newPos.y += linkOffset;
-            newPos.z -= ((i + 1)/ 100);
+            newPos.z -= ((i + 1)/ 100f);
+            Debug.Log("New Pos: " + newPos);
 
             GameObject newLink = Instantiate(chainLinkPrefab, newPos, Quaternion.identity, transform);
             HingeJoint2D hinge = newLink.GetComponent<HingeJoint2D>();
@@ -35,7 +40,32 @@ public class Chain : MonoBehaviour
             chain.Add(newLink);
         }
 
+        if (chainEndingPrefab != null) CreateChainEnding();
+        if (doubleEnded) CreateDoubleEnding();
+
         // Need to set this to false to avoid some jank
         chain[0].GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
+    }
+
+    private void CreateChainEnding()
+    {
+        Vector3 newPos = previousLink.position;
+        if (chainLength - 1 != 0) newPos.y += linkOffset;
+        newPos.z -= ((chainLength) / 100);
+
+        GameObject chainEnding = Instantiate(chainEndingPrefab, newPos, Quaternion.identity, transform);
+        HingeJoint2D hinge = chainEnding.GetComponent<HingeJoint2D>();
+        hinge.connectedBody = previousLink.GetComponent<Rigidbody2D>();
+
+        previousLink = chainEnding.transform;
+        chain.Add(chainEnding);
+
+        SpriteRenderer sprite = chainEnding.GetComponent<SpriteRenderer>();
+
+    }
+
+    private void CreateDoubleEnding()
+    {
+
     }
 }
